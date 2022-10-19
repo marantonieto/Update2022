3b46057ef678226837f00c2f014d18524d1385eb
Marantonieto <marcio.asfilho16@gmail.com>
2022-10-19 11:29:24 -0300

    Seeding service

--- Marantonieto/Startup.cs
+++ Marantonieto/Startup.cs
@@ -39,14 +39,17 @@ namespace Marantonieto
             services.AddDbContext<MarantonietoContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("MarantonietoContext"), builder =>
                         builder.MigrationsAssembly("Marantonieto")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
