ce1bbc18d9f25a293013dc1ef66380f5c017919d
Marantonieto <marcio.asfilho16@gmail.com>
2022-10-21 20:09:41 -0300

    simple create form

--- Marantonieto/Controllers/SellersController.cs
+++ Marantonieto/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Marantonieto.Services;
+using Marantonieto.Models;
 
 namespace Marantonieto.Controllers
 {
@@ -15,11 +16,22 @@ namespace Marantonieto.Controllers
         {
             _sellerService = service;
         }
-
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
